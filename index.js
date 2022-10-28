0474e0282c4b58046b49ce46f052cc2278bbddee
Nina Hawari <nina.hawari@gmail.com>
2022-10-28 18:23:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity){
+    let value = activity ? activity : "go to the office" 
+    return `This Monday, I will ${value}.`
+};
+
+function wrapAdjective (prompt = "*") {
+ 
+   return function(inner = "special") {
+        return `You are ${prompt}${inner}${prompt}!`;
+    };
+
+}
